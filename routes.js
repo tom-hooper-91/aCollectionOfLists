845c856eeee93b4642f4ed9ebd7a9231a93b08fd
Tom Hooper <tomwbhooper@hotmail.com>
2021-06-05 12:10:23 +1200

    update data.json

--- routes.js
+++ routes.js
@@ -4,14 +4,6 @@ const router = express.Router()
 const fs = require('fs')
 const lib = require('./lib.js')
 
-// router.get('', (req, res) => {
-//     fs.readFile(__dirname + '/data.json', (err, data) => {
-//         if (err) throw err
-//         const viewData = JSON.parse(data)
-//         res.render('home', viewData)
-//     })
-// })
-
 router.get('', (req, res) => {
     lib.readParse('/data.json', (viewData) => {
         res.render('home', viewData)
